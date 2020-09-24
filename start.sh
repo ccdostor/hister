1dc91923358ec94846503d2c4125fad0fff4bd9f
Mcl <mixool0204@gmail.com>
2020-09-24 21:23:55 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -21,10 +21,18 @@ header Upgrade    websocket
 path /gostpath
 }
 reverse_proxy @websocket_gost 127.0.0.1:2234
+
+@websocket_brook {
+header Connection *Upgrade*
+header Upgrade    websocket
+path /brookpath
+}
+reverse_proxy @websocket_gost 127.0.0.1:3234
 EOF
 
 
 # start
 caddy run --config /etc/caddy/Caddyfile --adapter caddyfile &
 ss-server -s 127.0.0.1 -p 1234 -k password -m chacha20-ietf-poly1305 --plugin /usr/bin/v2ray-plugin_linux_amd64 --plugin-opts "server;path=/sspath" &
-gost -L ss+ws://AEAD_CHACHA20_POLY1305:password@127.0.0.1:2234?path=/gostpath
+gost -L ss+ws://AEAD_CHACHA20_POLY1305:password@127.0.0.1:2234?path=/gostpath &
+brook wsserver -l 127.0.0.1:3234 --path /brookpath -p password
