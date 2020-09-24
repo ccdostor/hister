b79cb3c2bcca72cccb8d306c310ed787a8d12a3b
Mcl <mixool0204@gmail.com>
2020-09-24 22:47:37 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,28 +11,28 @@ file_server
 @websocket_ss {
 header Connection *Upgrade*
 header Upgrade    websocket
-path /sspath
+path $SSPATH
 }
 reverse_proxy @websocket_ss 127.0.0.1:1234
 
 @websocket_gost {
 header Connection *Upgrade*
 header Upgrade    websocket
-path /gostpath
+path $GOSTPATH
 }
 reverse_proxy @websocket_gost 127.0.0.1:2234
 
 @websocket_brook {
 header Connection *Upgrade*
 header Upgrade    websocket
-path /brookpath
+path $BROOKPATH
 }
 reverse_proxy @websocket_brook 127.0.0.1:3234
 
 @websocket_v2ray {
 header Connection *Upgrade*
 header Upgrade    websocket
-path /v2raypath
+path $V2RAYPATH
 }
 reverse_proxy @websocket_v2ray 127.0.0.1:4234
 EOF
@@ -45,7 +45,7 @@ cat << EOF > /v2ray.json
         {
             "port": 4234,"listen": "127.0.0.1","protocol": "vless",
             "settings": {"clients": [{"id": "8f91b6a0-e8ee-11ea-adc1-0242ac120002"}],"decryption": "none"},
-            "streamSettings": {"network": "ws","wsSettings": {"path": "/v2raypath"}}
+            "streamSettings": {"network": "ws","wsSettings": {"path": "$V2RAYPATH"}}
         }
     ],
     "outbounds": [{"protocol": "freedom"}]
@@ -59,7 +59,7 @@ caddy run --config /etc/caddy/Caddyfile --adapter caddyfile &
 
 # start
 caddy run --config /etc/caddy/Caddyfile --adapter caddyfile &
-ss-server -s 127.0.0.1 -p 1234 -k password -m chacha20-ietf-poly1305 --plugin /usr/bin/v2ray-plugin_linux_amd64 --plugin-opts "server;path=/sspath" &
-gost -L ss+ws://AEAD_CHACHA20_POLY1305:password@127.0.0.1:2234?path=/gostpath &
-brook wsserver -l 127.0.0.1:3234 --path /brookpath -p password &
+ss-server -s 127.0.0.1 -p 1234 -k $PASSWORD -m chacha20-ietf-poly1305 --plugin /usr/bin/v2ray-plugin_linux_amd64 --plugin-opts "server;path=/sspath" &
+gost -L ss+ws://AEAD_CHACHA20_POLY1305:$PASSWORD@127.0.0.1:2234?path=/gostpath &
+brook wsserver -l 127.0.0.1:3234 --path /brookpath -p $PASSWORD &
 /v2ray -config /v2ray.json
