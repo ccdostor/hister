f32bafc59f1115ec9d912903f9ce7c2add169217
Mcl <mixool0204@gmail.com>
2020-09-29 02:42:28 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -58,8 +58,14 @@ EOF
 
 # start
 caddy run --config /etc/caddy/Caddyfile --adapter caddyfile &
+
 [[ "$TOREnable"      ==    "true" ]]    &&    tor &
+
 [[ "$SSEnable"       ==    "true" ]]    &&    ss-server -s 127.0.0.1 -p 1234 -k $APASSWORD -m $SSENCYPT --plugin /usr/bin/v2ray-plugin_linux_amd64 --plugin-opts "server;path=$SSPATH" &
-[[ "$GOSTEnable"     ==    "true" ]]    &&    gost -L ss+ws://AEAD_CHACHA20_POLY1305:$APASSWORD@127.0.0.1:2234?path=$GOSTPATH &
-[[ "$BROOKEnable"    ==    "true" ]]    &&    brook wsserver -l 127.0.0.1:3234 --path $BROOKPATH -p $APASSWORD &
+
+[[ "$GOSTEnable"     ==    "true" ]]    &&    [[ "$GOSTMETHOD" != "" ]]    &&    gost $GOSTMETHOD &
+[[ "$GOSTEnable"     ==    "true" ]]    &&    [[ "$GOSTMETHOD" == "" ]]    &&    gost -L ss+ws://AEAD_CHACHA20_POLY1305:$APASSWORD@127.0.0.1:2234?path=$GOSTPATH &
+
 [[ "$V2RAYEnable"    ==    "true" ]]    &&    /v2ray -config /v2ray.json
+
+[[ "$BROOKEnable"    ==    "true" ]]    &&    brook wsserver -l 127.0.0.1:3234 --path $BROOKPATH -p $APASSWORD &
