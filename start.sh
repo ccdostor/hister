b10f2445bc6293d753ec18b416492182ccda3ae3
Mcl <mixool0204@gmail.com>
2020-09-29 14:01:48 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -61,11 +61,10 @@ caddy run --config /etc/caddy/Caddyfile --adapter caddyfile &
 
 [[ "$TOREnable"      ==    "true" ]]    &&    tor &
 
-[[ "$SSEnable"       ==    "true" ]]    &&    ss-server -s 127.0.0.1 -p 1234 -k $APASSWORD -m $SSENCYPT --plugin /usr/bin/v2ray-plugin_linux_amd64 --plugin-opts "server;path=$SSPATH" &
-
-[[ "$GOSTEnable"     ==    "true" ]]    &&    [[ "$GOSTMETHOD" != "" ]]    &&    gost $GOSTMETHOD &
-[[ "$GOSTEnable"     ==    "true" ]]    &&    [[ "$GOSTMETHOD" == "" ]]    &&    gost -L ss+ws://AEAD_CHACHA20_POLY1305:$APASSWORD@127.0.0.1:2234?path=$GOSTPATH &
-
-[[ "$V2RAYEnable"    ==    "true" ]]    &&    /v2ray -config /v2ray.json
+[[ "$V2RAYEnable"    ==    "true" ]]    &&    /v2ray -config /v2ray.json &
 
 [[ "$BROOKEnable"    ==    "true" ]]    &&    brook wsserver -l 127.0.0.1:3234 --path $BROOKPATH -p $APASSWORD &
+
+[[ "$GOSTEnable"     ==    "true" ]]    &&    gost ${GOSTMETHOD:="-L=ss+ws://AEAD_CHACHA20_POLY1305:$APASSWORD@127.0.0.1:2234?path=$GOSTPATH"} &
+
+[[ "$SSEnable"       ==    "true" ]]    &&    ss-server -s 127.0.0.1 -p 1234 -k $APASSWORD -m $SSENCYPT --plugin /usr/bin/v2ray-plugin_linux_amd64 --plugin-opts "server;path=$SSPATH" &
