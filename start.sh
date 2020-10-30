7bf3cced41d36e97dc17b0abd430820e121a99d7
Mcl <mixool0204@gmail.com>
2020-10-30 10:35:35 +0800

    trojan

--- start.sh
+++ start.sh
@@ -5,9 +5,8 @@ mkdir -p /etc/caddy/ /usr/share/caddy && wget $CADDYIndexPage -O /usr/share/cadd
 unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
 [[ "$ROBOTS" == "true" ]] && wget -qO- $ROBOTSCONFIG >/usr/share/caddy/robots.txt
 wget -qO- $CADDYCONFIG | sed -e "1c :$PORT" -e "s/\$SSPATH$/\\$SSPATH/" -e "s/\$GOSTPATH$/\\$GOSTPATH/" -e "s/\$BROOKPATH$/\\$BROOKPATH/" -e "s/\$VMESSPATH$/\\$VMESSPATH/" -e "s/\$VLESSPATH$/\\$VLESSPATH/" -e "s/\$TROJANPATH$/\\$TROJANPATH/" >/etc/caddy/Caddyfile
-wget -qO- $V2RAYCONFIG | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$password/$password/g" -e "s/\$VMESSPATH/\\$VMESSPATH/" -e "s/\$VLESSPATH/\\$VLESSPATH/" -e "s/\$TROJANPATH/\\$TROJANPATH/" >/v2ray.json
-cp /etc/caddy/Caddyfile /usr/share/caddy/
-cp /v2ray.json /usr/share/caddy/
+wget -qO- $V2RAYCONFIG | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$APASSWORD/$APASSWORD/g" -e "s/\$VMESSPATH/\\$VMESSPATH/" -e "s/\$VLESSPATH/\\$VLESSPATH/" -e "s/\$TROJANPATH/\\$TROJANPATH/" >/v2ray.json
+
 # start
 [[ "$TOREnable"      ==    "true" ]]    &&    tor &
 
