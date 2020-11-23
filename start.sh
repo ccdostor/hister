58038d3e102ccc2917a528d0a1fa2925d69b79cf
Mcl <mixool0204@gmail.com>
2020-11-23 12:39:33 +0800

    update

--- start.sh
+++ start.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 # vars
-AUUID-HASH=$(caddy hash-password --plaintext $AUUID)
+MYUUID-HASH=$(caddy hash-password --plaintext $AUUID)
 
 # configs
 mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
-wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$AUUID-HASH/$AUUID-HASH/g" >/etc/caddy/Caddyfile
+wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$MYUUID-HASH/g" >/etc/caddy/Caddyfile
 wget -qO- $CONFIGV2RAY | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" >/v2ray.json
 
 # storefiles
 mkdir -p /usr/share/caddy/$AUUID && wget -O /usr/share/caddy/$AUUID/StoreFiles $StoreFiles
-wget -P /usr/share/caddy/$AUUID -i /usr/share/caddy/$AUUID/StoreFiles 
+cd /usr/share/caddy/$AUUID       && wget -i /usr/share/caddy/$AUUID/StoreFiles 
 
 # start
 tor &
